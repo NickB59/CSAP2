16cf1750bbe47e50b6bf70170705b68bfcf8657c
NickB59 <b.nick312@gmail.com>
2018-02-25 21:55:00 -0800

    7

--- Unit7-2016/Unit7-Assignments/Lab11c.java
+++ Unit7-2016/Unit7-Assignments/Lab11c.java
@@ -6,15 +6,27 @@
 
 import static java.lang.System.*;
 
+import java.util.Scanner;
+
 public class Lab11c
 {
-   public static void main( String args[] )
-   {
+	public static void main( String args[] )
+	{
+	   Scanner keyboard = new Scanner(System.in);
+	    String choice="";
+	        do{
+	            out.print("Enter the size of the triangle : ");
+	            int big = keyboard.nextInt();
+	            out.print("Enter a letter : ");
+	            String value = keyboard.next();
 
+	                //instantiate a TriangleThree object
+	         TriangleThree tt = new TriangleThree( big, value );
+	            //call the toString method to print the triangle
+	            System.out.println( tt );
 
-
-
-
-
+	            System.out.print("Do you want to enter more data? ");
+	            choice=keyboard.next();
+	        }while(choice.equals("Y")||choice.equals("y"));
 	}
-}
+	}
--- Unit7-2016/Unit7-Assignments/TriangleThree.java
+++ Unit7-2016/Unit7-Assignments/TriangleThree.java
@@ -11,26 +11,43 @@ public class TriangleThree
    private int size;
    private String letter;
 
-	public TriangleThree()
-	{
-	}
+   public TriangleThree()
+   {
+   }
 
-	public TriangleThree(int count, String let)
-	{
-	}
+   public TriangleThree(int count, String let)
+   {
+       size = count;
+       letter = let;
+   }
 
-	public void setTriangle( String let, int sz )
-	{
-	}
+   public void setTriangle( String let, int sz )
+   {
+       size = sz;
+       letter = let;
+   }
 
-	public String getLetter()
-	{
-		return "#";
-	}
+   public String getLetter()
+   {
+       return letter;
+   }
 
-	public String toString()
-	{
-		String output="";
-		return output+"\n";
-	}
+   public String toString()
+   {
+       String output="";
+       for(int i = 1; i<=size; i++)
+       {
+           for(int k = i; k>0; k--)
+           {
+               output = output + letter + " ";
+           }
+           output= output + "\n";
+       }
+
+
+
+
+
+       return output+"\n";
+   }
 }
