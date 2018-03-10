b308db0b51ae22352259dc93f386af9ea2c8e0f2
NickB59 <b.nick312@gmail.com>
2018-03-09 21:52:52 -0800

    Unit 10

--- Unit10-2016/Unit10-Assignments/Lab16e/Lab16e.java
+++ Unit10-2016/Unit10-Assignments/Lab16e/Lab16e.java
@@ -15,14 +15,25 @@ public class Lab16e
 {
 	public static void main( String args[] ) throws IOException
 	{
-		Histogram test = new Histogram(new char[]{'a','b','c'},"lab16e1.dat");
+		Histogram test = new Histogram(new char[]{'a','b','c'},"C:\\Users\\bnick\\Desktop\\CSAP2\\Unit10-2016\\Unit10-Assignments\\Lab16e\\lab16e1.dat");
 		test.loadAndAnalyzeFile();
 		out.println("Letter occurring most : "+test.mostFrequent());
 		out.println("Letter occurring least : "+test.leastFrequent());
 		out.println("\n");
 		out.println(test);
-
-
-		//more test cases
+		
+		Histogram tes = new Histogram(new char[]{'d','e','a'},"C:\\Users\\bnick\\Desktop\\CSAP2\\Unit10-2016\\Unit10-Assignments\\Lab16e\\lab16e2.dat");
+		tes.loadAndAnalyzeFile();
+		out.println("Letter occurring most : "+tes.mostFrequent());
+		out.println("Letter occurring least : "+tes.leastFrequent());
+		out.println("\n");
+		out.println(tes);
+		
+		Histogram te = new Histogram(new char[]{'x', 'a', 'r', 'g'},"C:\\Users\\bnick\\Desktop\\CSAP2\\Unit10-2016\\Unit10-Assignments\\Lab16e\\lab16e3.dat");
+		te.loadAndAnalyzeFile();
+		out.println("Letter occurring most : "+te.mostFrequent());
+		out.println("Letter occurring least : "+te.leastFrequent());
+		out.println("\n");
+		out.println(te);
 	}
 }
